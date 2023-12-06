af11fa0b373948a9e58b4ef0f6afabf04555b364
dapgx <ubuntu@ip-192-168-1-56.ec2.internal>
2023-12-06 04:29:22 +0000

    test update

--- get_user_script/user_list.sh
+++ get_user_script/user_list.sh
@@ -4,3 +4,5 @@ while IFS=: read -r username _ uid gid _ groups; do
     groups=$(id -Gn $username | tr ' ' ',')
     echo "$username|$gid|$groups"
 done < /etc/passwd > all_users_list.csv
+
+#TESTING
