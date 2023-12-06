d2d3f0368c3f5ae42e109011253cab0ed7a1235f
Ubuntu <ubuntu@ip-192-168-1-56.ec2.internal>
2023-12-06 04:26:25 +0000

    add user list script

---
+++ get_user_script/user_list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while IFS=: read -r username _ uid gid _ groups; do
+    groups=$(id -Gn $username | tr ' ' ',')
+    echo "$username|$gid|$groups"
+done < /etc/passwd > all_users.csv
