c7b9325ed8e26243513821c595971b91a8f3cf60
Ubuntu <ubuntu@ip-192-168-1-56.ec2.internal>
2023-12-06 04:27:27 +0000

    little update

--- get_user_script/user_list.sh
+++ get_user_script/user_list.sh
@@ -3,4 +3,4 @@
 while IFS=: read -r username _ uid gid _ groups; do
     groups=$(id -Gn $username | tr ' ' ',')
     echo "$username|$gid|$groups"
-done < /etc/passwd > all_users.csv
+done < /etc/passwd > all_users_list.csv
